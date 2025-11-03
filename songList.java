ea0a715e4ed9777e8947e47d6c469191f816e5f7
wy-hunter <william.yang89@students.huntersoe.org>
2025-11-03 17:50:13 -0500

    Fixed GoalSet 1

--- songList.java
+++ songList.java
@@ -3,18 +3,19 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class songList {
-    public static String[][] songs;
+    public static String[][] songs = new String[100][];
 
     public static void main(String[] args) {
-        String filename = "datafile.txt";
+        String filename = "songList.txt";
         int lineNum = 0;
         try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
-            while (br.readLine() != null) {
-                String[] data = br.readLine().split(",");
-                for (int i = 0; i <= data.length; ++i) {
-                    songs[lineNum][i] = data[i];
-                }
-                lineNum += 1;
+            String line = br.readLine();
+            while (line != null) {
+                String[] data = line.split(",");
+                for (String s: data) System.out.println(s);
+                songs[lineNum] = data;
+                line = br.readLine();
+                lineNum++;
             }
         } catch (IOException e) {
             e.printStackTrace();
