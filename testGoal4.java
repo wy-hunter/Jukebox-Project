f3777a5af7edfdd15bcc12ea06363b74cacc5ceb
wy-hunter <william.yang89@students.huntersoe.org>
2025-11-21 02:11:40 -0500

    Goal Set 5 (part 1)
    Still working on ListView

--- testGoal4.java
+++ testGoal4.java
@@ -1,8 +1,16 @@
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+import javafx.scene.control.*;
+import javafx.event.*;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 // launches JavaFX and play from the queue with simple buttons
@@ -10,8 +18,7 @@ public class testGoal4 extends Application {
 
     private static purchaseQueue q;
     private static songPlayer sp;
-
-    private Label now;
+    private static balanceBox box;
 
     // what this does:
     // - Java entry point to start FX
@@ -32,7 +39,7 @@ public class testGoal4 extends Application {
         );
 
         // add some funds + enqueue a couple for demo
-        balanceBox box = new balanceBox();
+        box = new balanceBox();
         box.addFunds(1000);           // $10
         q.enqueue(0, box);            // Cruel Summer
         q.enqueue(1, box);            // The Spectre
@@ -52,7 +59,20 @@ public class testGoal4 extends Application {
     // - the primaryStage provided by FX
     @Override
     public void start(Stage primaryStage) {
-        now = new Label("Now Playing: " + sp.nowPlaying());
+        Group root = new Group();
+	    Scene scene = new Scene(root, 1000, 800, Color.WHITE);
+
+        Label now = new Label("Now Playing: " + sp.nowPlaying());
+        Label sortBy = new Label("Sort By: ");
+        Label credits = new Label("Credits: " + box.getFunds());
+        Label addAmount = new Label("Add funds: ");
+        TextField creditText = new TextField();
+
+        ChoiceBox sort = new ChoiceBox();
+        sort.getItems().addAll("Artist", "Title");
+
+        ChoiceBox paymentMethod = new ChoiceBox();
+        paymentMethod.getItems().addAll("Credit Card", "Cash");
 
         Button btnNext = new Button("Next");
         btnNext.setOnAction(e -> {
@@ -66,10 +86,23 @@ public class testGoal4 extends Application {
         Button btnResume = new Button("Resume");
         btnResume.setOnAction(e -> sp.resume());
 
-        VBox root = new VBox(10, now, btnNext, btnPause, btnResume);
-        root.setStyle("-fx-padding: 16;");
-        primaryStage.setTitle("Goal 4 Player");
-        primaryStage.setScene(new Scene(root, 300, 180));
+        Button btnAddCredits = new Button("Add Funds");
+        btnAddCredits.setOnAction(e -> {
+            box.addFunds(Integer.parseInt(creditText.getCharacters().toString()));
+            credits.setText("Credits: " + box.getFunds());
+        });
+
+        Button enqueue = new Button("Enqueue");
+        Button enqueueFront = new Button("Enqueue at Front");
+
+        HBox hb1 = new HBox(20, sortBy, sort);
+        HBox hb2 = new HBox(20, enqueue, enqueueFront);
+        HBox hb3 = new HBox(20, addAmount, creditText, btnAddCredits, paymentMethod);
+        VBox vb1 = new VBox(20, now, hb1, btnNext, btnPause, btnResume, hb2, credits, hb3);
+        root.getChildren().add(vb1);
+
+        primaryStage.setTitle("Song Player");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 }
